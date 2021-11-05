37df0869cb9852d31f1dc17ec1e16ad611eb675e
HoangMinh-dell7566 <sergioaguero369258@gmai.com>
2021-11-05 22:08:34 +0700

    update new project

---
+++ Bai14_STM32_ngatNgoai/MDK-ARM/RTE/RTE_Components.h
@@ -0,0 +1,14 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'STM32_TH_NgatNgoai' 
+ * Target:  'STM32_TH_NgatNgoai' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+#endif /* RTE_COMPONENTS_H */
