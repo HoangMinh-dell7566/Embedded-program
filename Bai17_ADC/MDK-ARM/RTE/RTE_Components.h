6223baaceb6d7d5ccea5e19afc376805b8c46e22
HoangMinh-dell7566 <sergioaguero369258@gmai.com>
2021-11-12 21:25:55 +0700

    update ADC

---
+++ Bai17_ADC/MDK-ARM/RTE/RTE_Components.h
@@ -0,0 +1,14 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'THSTM32_ADC_LM35' 
+ * Target:  'THSTM32_ADC_LM35' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+#endif /* RTE_COMPONENTS_H */
